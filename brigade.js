68a0e41eda8d5de4e539fcd7e4d4e8e81a48633b
Kent Rancourt <kent.rancourt@microsoft.com>
2021-02-22 14:10:53 -0500

    brigade.js makeover
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- brigade.js
+++ brigade.js
@@ -1,113 +1,180 @@
+// ============================================================================
+// NOTE: This is a Brigade 1.x script for now.
+// ============================================================================
+
 const { events, Job } = require("brigadier");
 const { Check } = require("@brigadecore/brigade-utils");
 
-const projectOrg = "brigadecore";
-const projectName = "brigade-github-app";
-
-const goImg = "brigadecore/go-tools:v0.1.0";
-const gopath = "/go";
-const localPath = gopath + `/src/github.com/${projectOrg}/${projectName}`;
-
 const releaseTagRegex = /^refs\/tags\/(v[0-9]+(?:\.[0-9]+)*(?:\-.+)?)$/;
 
-// **********************************************
-// Event Handlers
-// **********************************************
+const goImg = "brigadecore/go-tools:v0.1.0";
+const kanikoImg = "brigadecore/kaniko:v0.2.0";
+const helmImg = "brigadecore/helm-tools:v0.1.0";
+const localPath = "/workspaces/brigade-github-app";
 
-events.on("exec", (e, p) => {
-  return test().run();
-})
-
-events.on("push", (e, p) => {
-  let matchStr = e.revision.ref.match(releaseTagRegex);
-  if (matchStr) {
-    // This is an official release with a semantically versioned tag
-    let matchTokens = Array.from(matchStr);
-    let version = matchTokens[1];
-    return buildAndPublishImages(p, version).run();
+// MakeTargetJob is just a job wrapper around a make target.
+class MakeTargetJob extends Job {
+  constructor(target, img, e, env) {
+    super(target, img);
+    this.mountPath = localPath;
+    this. env = env || {};
+    this.env["SKIP_DOCKER"] = "true";
+    const matchStr = e.revision.ref.match(releaseTagRegex);
+    if (matchStr) {
+      this.env["VERSION"] = Array.from(matchStr)[1];
+    }
+    this.tasks = [
+      `cd ${localPath}`,
+      `make ${target}`
+    ];
   }
-  if (e.revision.ref == "refs/heads/master") {
-    // This runs tests then builds and publishes "edge" images
-    return test().run()
-    .then(() => {
-      buildAndPublishImages(p, "").run();
+}
+
+// PushImageJob is a specialized job type for publishing Docker images.
+class PushImageJob extends MakeTargetJob {
+  constructor(target, e, p) {
+    super(target, kanikoImg, e, {
+      "DOCKER_ORG": p.secrets.dockerhubOrg,
+      "DOCKER_USERNAME": p.secrets.dockerhubUsername,
+      "DOCKER_PASSWORD": p.secrets.dockerhubPassword
     });
   }
-})
+}
 
+// A map of all jobs. When a check_run:rerequested event wants to re-run a
+// single job, this allows us to easily find that job by name.
+const jobs = {};
+
+// Basic tests:
+
+const testUnitJobName = "test-unit";
+const testUnitJob = (e, p) => {
+  return new MakeTargetJob(testUnitJobName, goImg, e);
+}
+jobs[testUnitJobName] = testUnitJob;
+
+const lintJobName = "lint";
+const lintJob = (e, p) => {
+  return new MakeTargetJob(lintJobName, goImg, e);
+}
+jobs[lintJobName] = lintJob;
+
+// Docker images:
+
+const buildGatewayJobName = "build-gateway";
+const buildGatewayJob = (e, p) => {
+  return new MakeTargetJob(buildGatewayJobName, kanikoImg, e);
+}
+jobs[buildGatewayJobName] = buildGatewayJob;
+
+const pushGatewayJobName = "push-gateway";
+const pushGatewayJob = (e, p) => {
+  return new PushImageJob(pushGatewayJobName, e, p);
+}
+jobs[pushGatewayJobName] = pushGatewayJob;
+
+// Helm chart:
+
+const lintChartJobName = "lint-chart";
+const lintChartJob = (e, p) => {
+  return new MakeTargetJob(lintChartJobName, helmImg, e);
+}
+jobs[lintChartJobName] = lintChartJob;
+
+const publishChartJobName = "publish-chart";
+const publishChartJob = (e, p) => {
+  return new MakeTargetJob(publishChartJobName, helmImg, e, {
+    "HELM_REGISTRY": p.secrets.helmRegistry || "ghcr.io",
+    "HELM_ORG": p.secrets.helmOrg,
+    "HELM_USERNAME": p.secrets.helmUsername,
+    "HELM_PASSWORD": p.secrets.helmPassword
+  });
+}
+jobs[publishChartJobName] = publishChartJob;
+
+
+// Run the entire suite of tests, builds, etc. concurrently WITHOUT publishing
+// anything initially. If EVERYTHING passes AND this was a push (merge,
+// presumably) to the v2 branch, then run jobs to publish "edge" images.
+function runSuite(e, p) {
+  // Important: To prevent Promise.all() from failing fast, we catch and
+  // return all errors. This ensures Promise.all() always resolves. We then
+  // iterate over all resolved values looking for errors. If we find one, we
+  // throw it so the whole build will fail.
+  //
+  // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all#Promise.all_fail-fast_behaviour
+  return Promise.all([
+    // Basic tests:
+    run(e, p, testUnitJob(e, p)).catch((err) => { return err }),
+    run(e, p, lintJob(e, p)).catch((err) => { return err }),
+    // Docker images:
+    run(e, p, buildGatewayJob(e, p)).catch((err) => { return err }),
+    // Helm chart:
+    run(e, p, lintChartJob(e, p)).catch((err) => { return err })
+  ]).then((values) => {
+    values.forEach((value) => {
+      if (value instanceof Error) throw value;
+    });
+  }).then(() => {
+    if (e.revision.ref == "v2") {
+      // Push "edge" images.
+      Promise.all([
+        run(e, p, pushGatewayJob(e, p)).catch((err) => { return err })
+      ]).then((values) => {
+        values.forEach((value) => {
+          if (value instanceof Error) throw value;
+        }); 
+      })
+    }
+  });
+}
+
+// run the specified job, sandwiched between two other jobs to report status
+// via the GitHub checks API.
+function run(e, p, job) {
+  console.log("Check requested");
+  var check = new Check(e, p, job, `https://brigadecore.github.io/kashti/builds/${e.buildID}`);
+  return check.run();
+}
+
+// Either of these events should initiate execution of the entire test suite.
 events.on("check_suite:requested", runSuite);
 events.on("check_suite:rerequested", runSuite);
-events.on("check_run:rerequested", checkRequested);
+
+// These events MAY indicate that a maintainer has expressed, via a comment,
+// that the entire test suite should be run.
 events.on("issue_comment:created", (e, p) => Check.handleIssueComment(e, p, runSuite));
 events.on("issue_comment:edited", (e, p) => Check.handleIssueComment(e, p, runSuite));
 
-// **********************************************
-// Actions
-// **********************************************
-
-function test() {
-  // Create a new job to run Go tests
-  var job = new Job("tests", goImg);
-  job.mountPath = localPath;
-  // Set a few environment variables.
-  job.env = {
-      "SKIP_DOCKER": "true"
-  };
-  // Run Go unit tests
-  job.tasks = [
-    `cd ${localPath}`,
-    "make lint test"
-  ];
-  return job;
-}
-
-function buildAndPublishImages(project, version) {
-  let dockerRegistry = project.secrets.dockerhubRegistry || "docker.io";
-  let dockerOrg = project.secrets.dockerhubOrg || "brigadecore";
-  var job = new Job("build-and-publish-images", "docker:stable-dind");
-  job.privileged = true;
-  job.tasks = [
-    "apk add --update --no-cache make git",
-    "dockerd-entrypoint.sh &",
-    "sleep 20",
-    "cd /src",
-    `docker login ${dockerRegistry} -u ${project.secrets.dockerhubUsername} -p ${project.secrets.dockerhubPassword}`,
-    `DOCKER_REGISTRY=${dockerRegistry} DOCKER_ORG=${dockerOrg} VERSION=${version} make build-all-images push-all-images`,
-    `docker logout ${dockerRegistry}`
-  ];
-  return job;
-}
-
-// checkRequested is the default function invoked on a check_run:* event
-//
-// It determines which check is being requested (from the payload body)
-// and runs this particular check, or else throws an error if the check
-// is not found
-function checkRequested(e, p) {
-  payload = JSON.parse(e.payload);
-
-  // Extract the check name
-  name = payload.body.check_run.name;
-
-  // Determine which check to run
-  switch(name) {
-    case "tests":
-      return runTests(e, p, test);
-    default:
-      throw new Error(`No check found with name: ${name}`);
+// This event indicates a specific job is to be re-run.
+events.on("check_run:rerequested", (e, p) => {
+  const jobName = JSON.parse(e.payload).body.check_run.name;
+  const job = jobs[jobName];
+  if (job) {
+    return run(e, p, job(e, p));
   }
-}
+  throw new Error(`No job found with name: ${jobName}`);
+});
 
-// Here we can add additional Check Runs, which will run in parallel and
-// report their results independently to GitHub
-function runSuite(e, p) {
-  // For now, this is the one-stop shop running build, lint and test targets
-  return runTests(e, p, test);
-}
-
-// runTests is a Check Run that is run as part of a Checks Suite
-function runTests(e, p, jobFunc) {
-  var check = new Check(e, p, jobFunc(),
-    `https://brigadecore.github.io/kashti/builds/${e.buildID}`);
-  return check.run();
-}
+// Pushing new commits to any branch in github triggers a check suite. Such
+// events are already handled above. Here we're only concerned with the case
+// wherein a new TAG has been pushed-- and even then, we're only concerned with
+// tags that look like a semantic version and indicate a formal release should
+// be performed.
+events.on("push", (e, p) => {
+  const matchStr = e.revision.ref.match(releaseTagRegex);
+  if (matchStr) {
+    // This is an official release with a semantically versioned tag
+    return Group.runAll([
+      pushGatewayJob(e, p)
+    ])
+    .then(() => {
+      // All images built and published successfully, so build and publish the
+      // rest...
+      Group.runAll([
+        publishChartJob(e, p)
+      ]);
+    });
+  }
+  console.log(`Ref ${e.revision.ref} does not match release tag regex (${releaseTagRegex}); not releasing.`);
+});
